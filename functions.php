96bd3f8be851a366ed6da2011765a0d5a8bdc377
Sophie Kim <270010565@bun2-206-25.local>
2019-11-26 08:54:54 +1300

    logo customizer section added

--- functions.php
+++ functions.php
@@ -24,16 +24,16 @@ function register_navwalker(){
 }
 add_action( 'after_setup_theme', 'register_navwalker' );
 
-function extrahands_custom_logo_setup() {
-     $defaults = array(
-     'height'      => 40,
-     'width'       => 150,
-     'flex-height' => false,
-     'flex-width'  => false
-     );
- add_theme_support( 'custom-logo', $defaults );
-}
-add_action( 'after_setup_theme', 'extrahands_custom_logo_setup' );
+function extrahands_logo() {
+    $defaults = array(
+        'height'        => 40,
+        'width'         => 150,
+        'flex-height'   => false,
+        'flex-width'    => false
+    );
+  add_theme_support('custom-logo',$defaults);
+};
+add_action('after_setup_theme', 'extrahands_logo');
 
 require( get_template_directory() . '/inc/customizer.php' );
 
