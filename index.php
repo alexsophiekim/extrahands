56ce152d8dc1ede38f844c4e65251f2112139499
Sophie Kim <270010565@bun2-206-25.local>
2019-12-05 13:16:24 +1300

    index // front-page same

--- index.php
+++ index.php
@@ -20,15 +20,15 @@
 
 <?php if($allServices -> have_posts()): ?>
 <div class="container mt-5">
-  <section class="services">
+  <section class="services mb-4">
     <h3 class="text-center py-3">Services</h3>
     <div class="row">
      <?php while($allServices -> have_posts()): $allServices-> the_post(); ?>
-      <div class="col col-12 d-flex">
-            <?php the_content(); ?>
+      <div class="col col-12 d-flex serviceAlign">
+        <?php the_content(); ?>
         <p class="px-5"><?php echo get_post_meta(get_the_ID(), 'service_info', true); ?></p>
-        <button class="btn btn-outline-light btn-sm svc-btn" type="button" name="button" >
-            <a class="learnMore" href="services">Learn More</a>
+        <button class="btn btn-outline-light btn-sm svc-btn" type="button" name="button">
+            <a class="learnMore" href="index.php/services">Learn More</a>
         </button>
       </div>
     <?php endwhile; ?>
@@ -37,6 +37,58 @@
 </div>
 <?php endif; ?>
 
+
+
+<div class="bg-white">
+  <section class="testimonial">
+    <h3 class="text-center py-3 text-dark">Testimonial</h3>
+    <div class="row m-0">
+        <?php
+        for ($i=1; $i <= 5 ; $i++) {
+            if((get_theme_mod('extrahands_testimonial_img_'.$i)) ||(get_theme_mod('extrahands_testimonial_text_'.$i))){
+                $firstSlide = $i;
+                break;
+            }
+        }
+        ?>
+        <?php if(isset($firstSlide)): ?>
+            <div class="container">
+                <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <?php for ($i=1; $i <= 5 ; $i++) : ?>
+                            <?php if(get_theme_mod('extrahands_testimonial_img_'.$i)): ?>
+                          <li data-target="#carouselExampleCaptions" data-slide-to="<?php echo $i-1; ?>" class="<?php if($firstSlide === $i){ echo 'active';} ?>"></li>
+                        <?php endif; ?>
+                        <?php endfor; ?>
+                    </ol>
+                       <?php if(isset($firstSlide)): ?>
+                                   <div class="carousel-inner">
+                                      <?php for ($i=1; $i <= 5 ; $i++): ?>
+                                          <?php if(get_theme_mod('extrahands_testimonial_img_'.$i)): ?>
+                                             <div class="carousel-item <?php if($firstSlide === $i){ echo 'active';} ?>">
+                                                 <img src="<?php echo get_theme_mod( 'extrahands_testimonial_img_' . $i ); ?>" class="d-block carouselImg mx-auto" alt="testimonialImg">
+                                                 <div class="carousel-caption d-none d-md-block">
+                                                     <p class="text-dark font-weight-bold bg-white"><?php echo get_theme_mod('extrahands_testimonial_text_' .$i); ?></p>
+                                               </div>
+                                             </div>
+                                          <?php endif; ?>
+                                      <?php endfor; ?>
+                                   </div>
+                       <?php endif; ?>
+                <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+        <?php endif; ?>
+    </div>
+    </div>
+    </div>
+  </section>
+</div>
 
 <!-- Modal -->
 <div id="myModal" class="modal fade" role="dialog">
@@ -95,6 +147,5 @@
 
   </div>
 </div>
-
 
 <?php get_footer(); ?>
