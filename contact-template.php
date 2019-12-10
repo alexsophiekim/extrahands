5b54a9da054bc9defcacc493dec431da8480b153
Sophie Kim <270010565@bun2-206-25.local>
2019-12-10 14:11:48 +1300

    contact page edited

--- contact-template.php
+++ contact-template.php
@@ -33,55 +33,6 @@
   </div>
 </div>
 
-<?php if(is_page('contact')): ?>
-<div class="bg-white">
-  <div class="branchSet d-flex pt-4">
-  <?php if(has_nav_menu('branches_nav')): ?>
-      <div class="col-lg-12">
-          <?php
-            wp_nav_menu(array( 'theme_location' => 'branches_nav',
-                               'container' => '',
-                               'menu_class' => 'branchesNav'
 
-          ));
-           ?>
-      </div>
-    </div>
-  <?php endif; ?>
-
-</div>
-
-<div class="bg-white text-dark">
-    <?php if(is_page('contact')): ?>
-        <div class="container">
-          <h2 class="text-center"><?php the_title(); ?></h2>
-          <form class="contact" action="index.html" method="post">
-            <div class="row">
-              <div class="col-lg-6 col-sm-12">
-                <label for="inputName">Name</label>
-                <input type="text" class="form-control" placeholder="Name">
-                <label for="inputPhone">Phone</label>
-                <input type="Number" class="form-control" placeholder="Phone">
-                <label for="inputEmail4">Email</label>
-                <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-                <label for="inputAddress">Address</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-              </div>
-              <div class="col-lg-6 col-sm-12">
-                <label for="inputMessage">Message</label><br/>
-                <textarea name="Message" rows="10" cols="40"></textarea>
-              </div>
-            </div>
-            <div class="col-12 d-flex justify-content-center pb-5">
-              <button type="submit" class="btn btn-warning my-4">Submit</button>
-            </div>
-          </form>
-
-        </div>
-    <?php endif; ?>
-</div>
-
-
-<?php endif; ?>
 
  <?php get_footer(); ?>
